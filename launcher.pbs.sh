686b2bec2c5bbef1427a34bf595828c8e48719e4
Carmine Paolino <carmine@paolino.me>
2015-01-07 19:57:13 +0100

    Make the launcher more generic

--- launcher.pbs.sh
+++ launcher.pbs.sh
@@ -1,28 +1,28 @@
 # --
 # Launcher for PBS job scheduling systems.
 # To launch a job with this script:
-# $ qsub -v GENRETRON_PATH="$HOME/Genretron-Pylearn2",EXPERIMENT="single_experiments/mlp_20150107A.yaml" launcher.pbs.sh
+# $ qsub -lwalltime=3:00:0 \
+#     -v GENRETRON_PATH="$HOME/Genretron-Pylearn2",\
+#     EXPERIMENT="single_experiments/mlp_20150107A.yaml" \
+#     -o $HOME/Experiments/Genretron-Pylearn2/mlp_20150107A.log \
+#     -j oe launcher.pbs.sh
 # --
 # shell for the job:
 #PBS -S /bin/bash
-# job requires at most 3 hours, 0 minutes
-#     and 0 seconds wallclock time and uses one 16-core node:
-#PBS -lwalltime=3:00:00 -lnodes=1:cores16
-# cd to the directory where the program is to be called:
-if [ "x" == "x$GENRETRON_PATH" ]
-then echo "GENRETRON_PATH variable not set!! Quitting!"
-else cd $GENRETRON_PATH
-fi
+# job uses one 16-core node:
+#PBS -lnodes=1:cores16
 # load the Intel Compilers and Math Kernel Library
 module load fortran mkl
 module load fortran/intel
 module load c/intel
 # request that threads should remain on the same core:
 module load paffinity
-# activate the python virtualenv
-. env/bin/activate
-# launch the learning procedure
-if [ "x" == "x$EXPERIMENT" ]
-then echo "EXPERIMENT variable not set!! Quitting!"
-else pylearn2/pylearn2/scripts/train.py $EXPERIMENT
+# check if environment variables are correctly set
+if [ "x" == "x$GENRETRON_PATH" || "x" == "x$EXPERIMENT" ]
+then echo "One or more variable are not set!! Quitting!"
+exit 1
 fi
+# activate the python virtualenv
+. ${GENRETRON_PATH}/env/bin/activate
+# launch the learning procedure
+${GENRETRON_PATH}/pylearn2/pylearn2/scripts/train.py $EXPERIMENT
