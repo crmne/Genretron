ee7ca7617dc1f8f04b9dcad340ef7d908ae8e823
Carmine Paolino <carmine@paolino.me>
2015-05-03 17:54:36 +0200

    Prediction for vectorspaces and conv2dspaces

--- scripts/predict.py
+++ scripts/predict.py
@@ -45,21 +45,29 @@ def predict(model_path, track_paths, verbose=False):
         model.dataset_yaml_src, instantiate=False).keywords
     seconds = dataset_params.get('seconds', None)
     valid_spectrogram_params = [
-            'window_size', 'step_size', 'window_type', 'fft_resolution']
+        'window_size', 'step_size', 'window_type', 'fft_resolution'
+    ]
     spectrogram_params = \
         {k: dataset_params.get(k, None) for k in valid_spectrogram_params}
+    invalid_dataset_params = ['which_set', 'verbose', 'print_params']
+    for invalid_key in invalid_dataset_params:
+        if dataset_params.get(invalid_key, False):
+            del dataset_params[invalid_key]
     dataset = GTZAN(which_set='all',
-                    seconds=seconds,
-                    print_params=verbose,
                     verbose=verbose,
-                    **spectrogram_params)
+                    print_params=False,
+                    **dataset_params)
 
     def space_converter(x):
         return dataset.spaces_converters[dataset.converter]((x, []))[0]
 
     def view_converter(x):
-        return dataset.view_converters[dataset.converter].\
-            design_mat_to_topo_view(x)
+        vc = dataset.view_converters[dataset.converter]
+        if vc is None:
+            return x
+        else:
+            return dataset.view_converters[dataset.converter].\
+                design_mat_to_topo_view(x)
 
     genres = dataset.genres
 
@@ -76,7 +84,12 @@ def predict(model_path, track_paths, verbose=False):
         x = view_converter(space_converter(spectrogram))
 
         # get the results
-        y = f(x)[0]
+        if dataset.space == "conv2d":
+            y = f(x)[0]
+        elif dataset.space == "vector":
+            y = f(x).mean(axis=0)
+        else:
+            raise NotImplementedError
 
         predictions = sorted(
             [(genres[i], v * 100) for i, v in enumerate(y)],
