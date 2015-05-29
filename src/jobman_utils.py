6acc11713b0a7c6664df0ba0c66c8686f5446c18
Carmine Paolino <carmine@paolino.me>
2015-05-29 14:44:51 +0200

    Separating jobman utils code from generic utils

---
+++ src/jobman_utils.py
@@ -0,0 +1,52 @@
+import numpy
+from jobman.tools import DD
+
+
+def results_extractor(train_obj):
+    channels = train_obj.model.monitor.channels
+    epoch_time = channels['valid_y_misclass'].time_record
+    validyma = [i.item() for i in channels['valid_y_misclass'].val_record]
+    testyma = [i.item() for i in channels['test_y_misclass'].val_record]
+    trainyma = [i.item() for i in channels['train_y_misclass'].val_record]
+    train_time = epoch_time[-1]
+    validym = validyma[-1]
+    testym = testyma[-1]
+    trainym = trainyma[-1]
+
+    return DD(
+        epoch_time=epoch_time,
+        valid_y_misclass_array=validyma,
+        test_y_misclass_array=testyma,
+        train_y_misclass_array=trainyma,
+        train_time=train_time,
+        valid_y_misclass=validym,
+        test_y_misclass=testym,
+        train_y_misclass=trainym,
+    )
+
+
+def log_uniform(low, high):
+    """
+    Generates a number that's uniformly distributed in the log-space between
+    `low` and `high`
+
+    Parameters
+    ----------
+    low : float
+        Lower bound of the randomly generated number
+    high : float
+        Upper bound of the randomly generated number
+
+    Returns
+    -------
+    rval : float
+        Random number uniformly distributed in the log-space specified by `low`
+        and `high`
+    """
+    log_low = numpy.log(low)
+    log_high = numpy.log(high)
+
+    log_rval = numpy.random.uniform(log_low, log_high)
+    rval = float(numpy.exp(log_rval))
+
+    return rval
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
 import numpy
 import sys
-from jobman.tools import DD
 
 
 def filter_keys_from_dict(keys, dict):
@@ -10,56 +9,6 @@ def filter_keys_from_dict(keys, dict):
 
 def filter_null_args(**kwargs):
     return {k: v for k, v in kwargs.items() if v is not None}
-
-
-def results_extractor(train_obj):
-    channels = train_obj.model.monitor.channels
-    epoch_time = channels['valid_y_misclass'].time_record
-    validyma = [i.item() for i in channels['valid_y_misclass'].val_record]
-    testyma = [i.item() for i in channels['test_y_misclass'].val_record]
-    trainyma = [i.item() for i in channels['train_y_misclass'].val_record]
-    train_time = epoch_time[-1]
-    validym = validyma[-1]
-    testym = testyma[-1]
-    trainym = trainyma[-1]
-
-    return DD(
-        epoch_time=epoch_time,
-        valid_y_misclass_array=validyma,
-        test_y_misclass_array=testyma,
-        train_y_misclass_array=trainyma,
-        train_time=train_time,
-        valid_y_misclass=validym,
-        test_y_misclass=testym,
-        train_y_misclass=trainym,
-    )
-
-
-def log_uniform(low, high):
-    """
-    Generates a number that's uniformly distributed in the log-space between
-    `low` and `high`
-
-    Parameters
-    ----------
-    low : float
-        Lower bound of the randomly generated number
-    high : float
-        Upper bound of the randomly generated number
-
-    Returns
-    -------
-    rval : float
-        Random number uniformly distributed in the log-space specified by `low`
-        and `high`
-    """
-    log_low = numpy.log(low)
-    log_high = numpy.log(high)
-
-    log_rval = numpy.random.uniform(log_low, log_high)
-    rval = float(numpy.exp(log_rval))
-
-    return rval
 
 
 def query_yes_no(question, default="yes"):
