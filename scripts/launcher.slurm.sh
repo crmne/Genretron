43b977cfbdc2715f1f76136f02badda032627579
Carmine Paolino <carmine@paolino.me>
2015-05-29 15:05:59 +0200

    Generalize slurm launcher

--- scripts/launcher.slurm.sh
+++ scripts/launcher.slurm.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-#SBATCH -p gpu_short
-#SBATCH -t 30
-#SBATCH -N 5
 . ~/.bashrc
 export THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32
 export DB_TABLE=$1
 
-srun $GENRETRON_PATH/env/bin/jobman sql -n 1 $DB_TABLE $GENRETRON_PATH/results
+$GENRETRON_PATH/env/bin/jobman sql -n 1 $DB_TABLE $GENRETRON_PATH/results
