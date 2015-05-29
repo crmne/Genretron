7474a61ac8e63041db7d770ae7161a8cb62bc2f1
Carmine Paolino <carmine@paolino.me>
2015-05-29 17:24:18 +0200

    Use srun in slurm launcher

--- scripts/launcher.slurm.sh
+++ scripts/launcher.slurm.sh
@@ -3,4 +3,4 @@
 export THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32
 export DB_TABLE=$1
 
-$GENRETRON_PATH/env/bin/jobman sql -n 1 $DB_TABLE $GENRETRON_PATH/results
+srun $GENRETRON_PATH/env/bin/jobman sql -n 1 $DB_TABLE $GENRETRON_PATH/results
