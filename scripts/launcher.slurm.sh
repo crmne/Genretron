627008bfad162d90d0fc5921c0df117ff1b510c5
Carmine Paolino <carmine@paolino.me>
2015-06-22 14:32:24 +0200

    more explicit vars

--- scripts/launcher.slurm.sh
+++ scripts/launcher.slurm.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 . ~/.bashrc
 
-if [ -z "$GENRETRON_PATH" ] || [ -z "$DB_TABLE" ] || [ -z "$1" ]
+export CONSECUTIVE_JOBS=$1
+export DB_TABLE=$2
+
+
+if [ -z "$GENRETRON_PATH" ] || [ -z "$DB_TABLE" ] || [ -z "$CONSECUTIVE_JOBS" ]
     echo "One of the required variables by this script has not been set. Exiting"
     then exit 1
 fi
 
 . $GENRETRON_PATH/env/bin/activate
 
-COMMAND="$GENRETRON_PATH/env/bin/jobman sql -n $1 $DB_TABLE $GENRETRON_PATH/results"
+COMMAND="$GENRETRON_PATH/env/bin/jobman sql -n $CONSECUTIVE_JOBS $DB_TABLE $GENRETRON_PATH/results"
 
 echo $COMMAND
 srun $COMMAND
