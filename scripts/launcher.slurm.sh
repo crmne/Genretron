82580f84e4aa03310f58af3b658b9736ee1a7605
Carmine Paolino <carmine@paolino.me>
2015-06-22 14:22:17 +0200

    Fail if a required variable has not been set

--- scripts/launcher.slurm.sh
+++ scripts/launcher.slurm.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 . ~/.bashrc
 
+if [ -z "$GENRETRON_PATH" ] || [ -z "$DB_TABLE" ] || [ -z "$1" ]
+    echo "One of the required variables by this script has not been set. Exiting"
+    then exit 1
+fi
+
 . $GENRETRON_PATH/env/bin/activate
 srun $GENRETRON_PATH/env/bin/jobman sql -n $1 $DB_TABLE $GENRETRON_PATH/results
