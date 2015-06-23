9dd4dfd1ba860fdafe259a67043ec32f4fbf7241
Carmine Paolino <carmine@paolino.me>
2015-06-23 16:26:45 +0200

    Read jobman conf from yaml

--- scripts/jobman_sqlschedule.py
+++ scripts/jobman_sqlschedule.py
@@ -3,36 +3,11 @@ import sys
 import argparse
 import jobman
 import itertools
+import yaml
 from pylearn2.scripts.jobman.experiment import ydict, train_experiment
 
-hp = {
-    'learning_rate': .001,
-    'batch_size': [10, 100],
-    'h0_output_channels': [16, 32, 64],
-    'h0_kernel_shape_x': 513,
-    'h0_kernel_shape_y': [4, 8, 16, 32],
-    'h0_pool_shape_x': 1,
-    'h0_pool_shape_y': 4,
-    'h0_pool_stride_x': 1,
-    'h0_pool_stride_y': 2,
-    'h1_kernel_shape_x': 1,
-    'h1_kernel_shape_y': [4, 8, 16, 32],
-    'h1_pool_shape_x': 1,
-    'h1_pool_shape_y': 4,
-    'h1_pool_stride_x': 1,
-    'h1_pool_stride_y': 2,
-    'h1_output_channels': [32, 64],
-    'h2_kernel_shape_x': 1,
-    'h2_kernel_shape_y': [4, 8, 16, 32],
-    'h2_pool_shape_x': 1,
-    'h2_pool_shape_y': 4,
-    'h2_pool_stride_x': 1,
-    'h2_pool_stride_y': 2,
-    'h2_output_channels': 64
-}
 
-
-def generate_hyperparameters(hyperparameters):
+def generate_hyper_parameters(hyperparameters):
     dd = jobman.DD()
     fixed_hyperparameters = \
         [(k, v) for k, v in hyperparameters.items() if not isinstance(v, list)]
@@ -49,7 +24,7 @@ def generate_hyperparameters(hyperparameters):
 
 def make_argument_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('yaml_path')
+    parser.add_argument('yaml_config_path')
     parser.add_argument('db_path')
     parser.add_argument('table_name')
     return parser
@@ -63,22 +38,28 @@ if __name__ == '__main__':
     full_db_path = "{0}?table={1}".format(args.db_path, args.table_name)
     db = jobman.api0.open_db(full_db_path)
 
-    # read yaml template
-    with open(args.yaml_path) as f:
-        yaml_template = f.read()
+    # read config
+    with open(args.yaml_config_path) as f:
+        config = yaml.load(f.read())
 
+    yaml_template_path = config['yaml_template']
+    extract_results_function = config['extract_results']
+    hyper_parameters = config['hyper_parameters']
+
+    sys.stdout.write('Generating experiments and sending them to the db')
     # generate experiments and put them in db
-    for hyperparameters in generate_hyperparameters(hp):
+    for h in generate_hyper_parameters(hyper_parameters):
         state = jobman.DD()
-        state.yaml_template = yaml_template
+        state.yaml_template = yaml_template_path
         state.hyper_parameters = \
-            jobman.expand(jobman.flatten(hyperparameters), dict_type=ydict)
-        state.extract_results = "jobman_utils.results_extractor"
+            jobman.expand(jobman.flatten(h), dict_type=ydict)
+        state.extract_results = extract_results_function
         jobman.sql.insert_job(
             train_experiment,
             jobman.flatten(state),
             db)
         sys.stdout.write('.')
+        sys.stdout.flush()
     print('')
 
     # create view
